fed2f13434f1aa4e5dea495e9852a784d7bcd171
Sergey Grigoriev <sergey.grigoriev@sbb.ch>
2024-07-10 13:34:08 +0200

    feat: initial implementation

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
-# Polarion ALM extension to <...>
+# Markdown to HTML Maven Plugin
 
-This Polarion extension provides possibility to <...>
+This Maven plugin uses GitHub API for converting Markdown to HTML.
+
 ## Build
 
 This extension can be produced using maven:
@@ -8,29 +9,10 @@ This extension can be produced using maven:
 mvn clean package
 ```
 
-## Installation to Polarion
-
-To install the extension to Polarion `ch.sbb.polarion.extension.<extension_name>-<version>.jar`
-should be copied to `<polarion_home>/polarion/extensions/ch.sbb.polarion.extension.<extension_name>/eclipse/plugins`
-It can be done manually or automated using maven build:
-```bash
-mvn clean install -P install-to-local-polarion
-```
-For automated installation with maven env variable `POLARION_HOME` should be defined and point to folder where Polarion is installed.
-
-Changes only take effect after restart of Polarion.
-
-## Polarion configuration
-
-<...>
-
-
-## Extension Configuration
-
-<...>
-
-
 ## Usage
 
-<...>
+This plugin can be used in a maven project by adding the following to the `pom.xml`:
 
+```xml
+
+```
---
+++ src/main/java/ch/sbb/markdown2html/github/GitHubHttpClient.java
@@ -0,0 +1,56 @@
+package ch.sbb.markdown2html.github;
+
+import ch.sbb.markdown2html.github.model.MarkdownRequest;
+import lombok.AllArgsConstructor;
+import org.apache.hc.client5.http.classic.methods.HttpPost;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.core5.http.HttpHeaders;
+import org.apache.hc.core5.http.HttpStatus;
+import org.apache.hc.core5.http.io.HttpClientResponseHandler;
+import org.apache.hc.core5.http.io.entity.EntityUtils;
+import org.apache.hc.core5.http.io.entity.StringEntity;
+
+import java.io.IOException;
+
+@AllArgsConstructor
+public class GitHubHttpClient {
+    private static final String GITHUB_API_URL = "https://api.github.com/markdown";
+    private static final String ACCEPT_HEADER = "application/vnd.github+json";
+    private static final String API_VERSION_HEADER = "X-GitHub-Api-Version";
+    private static final String API_VERSION = "2022-11-28";
+    private static final String AUTHORIZATION_HEADER = "Authorization";
+    private static final String GFM = "gfm";
+
+    private final String githubToken;
+
+    public String convertMarkdownToHtml(String markdown) throws IOException {
+        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
+            HttpPost httpPost = new HttpPost(GITHUB_API_URL);
+            httpPost.setHeader(HttpHeaders.ACCEPT, ACCEPT_HEADER);
+            httpPost.setHeader(API_VERSION_HEADER, API_VERSION);
+
+            if (githubToken != null && !githubToken.isEmpty()) {
+                httpPost.setHeader(AUTHORIZATION_HEADER, "Bearer " + githubToken);
+            }
+
+            MarkdownRequest markdownRequest = new MarkdownRequest(GFM, markdown);
+            httpPost.setEntity(new StringEntity(markdownRequest.toJSON()));
+
+            return executeRequest(httpClient, httpPost);
+        }
+    }
+
+    private String executeRequest(CloseableHttpClient httpClient, HttpPost httpPost) throws IOException {
+        HttpClientResponseHandler<String> responseHandler = response -> {
+            int statusCode = response.getCode();
+            if (statusCode == HttpStatus.SC_OK) {
+                return EntityUtils.toString(response.getEntity());
+            } else {
+                throw new IllegalStateException("HTTP request failed with status: " + statusCode);
+            }
+        };
+
+        return httpClient.execute(httpPost, responseHandler);
+    }
+}
